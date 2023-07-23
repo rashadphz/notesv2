86c13ed9cb1fe2f086510d7a11c0b35e4fb113b3
Rashad Philizaire <rashadphilizaire@gmail.com>
2023-07-22 22:31:30 -0400

    removed old renderer

--- src/renderer/redux/hooks.ts
+++
@@ -1,8 +0,0 @@
-import type { TypedUseSelectorHook } from "react-redux";
-import { useDispatch, useSelector } from "react-redux";
-
-import type { AppDispatch, RootState } from "./store";
-
-export const useReduxDispatch = () => useDispatch<AppDispatch>();
-export const useReduxSelector: TypedUseSelectorHook<RootState> =
-  useSelector;
