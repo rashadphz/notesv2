4261e220064b43bf9bd5b58ad6d11d768e20f0f7
Rashad Philizaire <rashadphilizaire@gmail.com>
2023-08-06 03:34:56 -0400

    better opacity on command modal

--- frontend/src/components/commandModal/index.tsx
+++ frontend/src/components/commandModal/index.tsx
@@ -72,7 +72,7 @@ const CommandModal = () => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-25 transition-opacity" />
+          <div className="fixed inset-0 bg-base-200/80 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 overflow-y-auto p-4 sm:p-6 md:p-20">
